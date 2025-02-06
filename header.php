aa8516daccf2c401f8b0b8c3c88cd7cd27198eb3
Isuru Akalanka <jmiakalanka@gmail.com>
2025-02-06 14:37:16 +0530

    Fix favicon related issues

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Intern Go</title>
-    <link rel="icon" type="image/png" href="images/favicon.png">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
